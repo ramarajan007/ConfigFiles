aa533fe211a048e08dd3c83bd820e805e483376c
ramarajan007 <ramarajan581995@gmail.com>
2024-09-05 15:02:17 +0530

    Update apache.sh

--- Apache Config Files/apache.sh
+++ Apache Config Files/apache.sh
@@ -1,1 +1,3 @@
 sed -i 's/^        Options Indexes FollowSymLinks/        Options -Indexes -FollowSymLinks/' /etc/apache2/apache2.conf
+sed -i 's/^ServerTokens Full/ServerTokens Prod/' /etc/apache2/conf-available/security.conf
+sed -i 's/^ServerSignature On/ServerSignature Off/' /etc/apache2/conf-available/security.conf
